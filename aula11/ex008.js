4fe47035a2567e6a8efd40e2a697a7ed1963ec86
Renan Barbosa Tirapelle <106815029+retirapelle@users.noreply.github.com>
2023-08-10 19:45:25 -0300

    fiz os exercícios da aula 11

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 61
+console.log(`A velocidade do seu carro é ${vel}KM/h`)
+if (vel > 60) {
+    console.log(`Você ultrapassou o limite de velocidade permitido. MULTADO!`)
+} else {
+    console.log(`Você está dentro do limite permito. Dirija com cuidado!`)
+}
+console.log(`Use sempre o cinto de segurança.`)
