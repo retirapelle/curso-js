cde63f032321c82bdf37ddbf6c6cc09053ee39a4
Renan Barbosa Tirapelle <106815029+retirapelle@users.noreply.github.com>
2023-08-21 11:53:43 -0300

    while/do...while
    meu primeiro código com while/do...while no estudo de repetições dentro do JS

---
+++ aula13/ambiente.js
@@ -0,0 +1,23 @@
+var c = 1 
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6) // Esta maneira, executamos primeiro o bloco e depois o teste é aplicado
+
+/*
+var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++
+}
+Esta maneira, o teste é feito antes da execução do bloco
+*/
+
+/* essa maneira é de bobo
+console.log('Passo 1')
+console.log('Passo 2')
+console.log('Passo 3')
+console.log('Passo 4')
+console.log('Passo 5')
+console.log('Passo 6')
+*/
